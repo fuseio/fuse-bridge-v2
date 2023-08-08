f98343b37066e85020b453b175fbb68c85e85e65
Mayank Mittal <msquare.pvt21@gmail.com>
2023-08-08 19:35:27 +0530

    update WrappedErc20

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,6 @@ require("solidity-coverage");
 require('hardhat-gas-reporter');
 require('hardhat-deploy');
 require('hardhat-deploy-ethers');
-require('@openzeppelin/hardhat-upgrades');
 require('./tasks');
 
 // This is a sample Hardhat task. To learn how to create your own go to
