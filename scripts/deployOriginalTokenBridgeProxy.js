3b639207c0757197d3a442e607b02d7baa5ec6a7
Mayank Mittal <msquare.pvt21@gmail.com>
2023-08-21 22:18:11 +0530

    Update EndtoEnd tests

--- scripts/deployOriginalTokenBridgeProxy.js
+++ scripts/deployOriginalTokenBridgeProxy.js
@@ -2,10 +2,11 @@
 const { ethers, upgrades } = require("hardhat")
 
 async function main() {
-    const Bridge = await ethers.getContractFactory("contracts/OriginalTokenBridge.sol:OriginalTokenBridge")
-    const bridge = await upgrades.deployProxy(Bridge, ["0x9740FF91F1985D8d2B71494aE1A2f723bb3Ed9E4", 145, "0x0BE9e53fd7EDaC9F859882AfdDa116645287C629"], { kind: "uups" })
+    const Bridge = await ethers.getContractFactory("contracts/OriginalTokenBridgeUpgradable.sol:OriginalTokenBridgeUpgradable")
+    const bridge = await upgrades.deployProxy(Bridge, ["0x9740FF91F1985D8d2B71494aE1A2f723bb3Ed9E4", 110, "0x0BE9e53fd7EDaC9F859882AfdDa116645287C629"], { kind: "uups" })
     console.log("Deploying bridge...")
     await bridge.waitForDeployment()
+    console.log(bridge.deploymentTransaction().hash)
     console.log("Bridge deployed to:", await bridge.getAddress())
 }
 
--- scripts/deployWrappedTokenBridgeProxy.js
+++ scripts/deployWrappedTokenBridgeProxy.js
@@ -3,8 +3,8 @@
 const { ethers, upgrades } = require("hardhat")
 
 async function main() {
-    const Bridge = await ethers.getContractFactory("contracts/WrappedTokenBridge.sol:WrappedTokenBridge")
-    const bridge = await upgrades.deployProxy(Bridge, ["0x9740FF91F1985D8d2B71494aE1A2f723bb3Ed9E4"], { kind: "uups" })
+    const Bridge = await ethers.getContractFactory("contracts/WrappedTokenBridgeUpgradable.sol:WrappedTokenBridgeUpgradable")
+    const bridge = await upgrades.deployProxy(Bridge, ["0x3c2269811836af69497E5F486A85D7316753cf62"], { kind: "uups" })
     console.log("Deploying bridge...")
     await bridge.waitForDeployment()
     console.log("Bridge deployed to:", await bridge.getAddress())
